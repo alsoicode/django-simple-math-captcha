e4a538774aa2cf1a0f5feb68bb8d48a295f2f6bc
Brandon Taylor <alsoicode@gmail.com>
2014-03-18 22:17:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=README,
     author='Brandon Taylor',
     author_email='alsoicode@gmail.com',
-    url='http://brandonftaylor.com/',
+    url='https://brandonftaylor.com/',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
